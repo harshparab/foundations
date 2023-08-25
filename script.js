da3a4879261e0683ce8c200f451b6379829707ec
harshparab <harsh_parab@outlook.com>
2023-08-25 21:11:13 +0530

    completed foundations

---
+++ script.js
@@ -0,0 +1,21 @@
+function sayHello(name) {
+  console.log(`Hello, ${name}`);
+}
+
+sayHello("Harsh");
+
+function subtract(numOne, numTwo) {
+  return numOne - numTwo;
+}
+
+console.log(subtract(5, 3));
+
+const birthYear = 1998;
+
+let favoriteFoods = ["Biryani", "Dosa", "Fish Curry"];
+
+let bookDetails = {
+  title: "The Hitchhiker's Guide to the Galaxy",
+  author: "Douglas Adams",
+  publicationYear: 1979,
+};
